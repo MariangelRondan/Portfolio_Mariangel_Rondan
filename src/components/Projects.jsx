3cc81eaa5825c26e35f8a38d478caa04240c013f
MariangelRondan <rondanmariangel@gmail.com>
2024-03-07 11:59:17 -0300

    projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import ProjectItem from "./ProjectItem"
+import RickAndMorty from '../assets/rickandMorty.png'
+import hyperMegaRed from '../assets/hyper-mega-red.png'
+import jap from "../assets/e-commerce.png"
+import drivers from "../assets/drivers-photo.png"
 
 const Projects = () => {
 
     return (
-        <div>Projects</div>
+        <div id="projects" className="max-w-[1048px] m-auto md:pl-20 p-4 py-16">
+            <h1 className="text-4xl font-bold text-center text-[#00044]">Projects</h1>
+            <p className="text-center py-8">no se que poner aqui</p>
+      <div className="grid sm:grid-cols-2 gap-12">
+        <ProjectItem   img={hyperMegaRed} title='hyperMegaRed'/>
+        <ProjectItem  img={jap} title='jap'/>
+        <ProjectItem  img={drivers} title='drivers'/>
+        <ProjectItem  img={RickAndMorty} title='RickAndMorty'/>
+      </div>
+      
+      
+        </div>
+        
     )
 }
 
