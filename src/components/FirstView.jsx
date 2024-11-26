d79b99726ea7a780300436f13a2f71047bf34c13
Mariangel <rondanmariangel@gmail.com>
2024-11-26 20:46:58 -0300

    start of something new

--- src/components/FirstView.jsx
+++ src/components/FirstView.jsx
@@ -1,171 +1,68 @@
 import React, { useEffect } from "react";
 import resume from "../assets/MyResume.svg";
+import water from "../assets/water.png";
+
 import MyResume from "../assets/MariangelRondan-resume.pdf";
-import styles from "./FirstView.module.css";
 import { TypeAnimation } from "react-type-animation";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 function FirstView() {
-  useEffect(() => {
-    const interBubble = document.getElementById("interactive");
-    if (!interBubble) return;
-
-    let curX = 0;
-    let curY = 0;
-    let tgX = 0;
-    let tgY = 0;
-
-    const move = () => {
-      curX += (tgX - curX) / 20;
-      curY += (tgY - curY) / 20;
-      interBubble.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
-      requestAnimationFrame(move);
-    };
-
-    const handleMouseMove = (event) => {
-      tgX = event.clientX;
-      tgY = event.clientY;
-    };
-
-    window.addEventListener("mousemove", handleMouseMove);
-    move();
-
-    // Cleanup
-    return () => {
-      window.removeEventListener("mousemove", handleMouseMove);
-    };
-  }, []);
-
   return (
     <div id="main">
-      <div className={styles.card}>
-        <svg
-          viewBox="0 0 100% 100%"
-          xmlns="http://www.w3.org/2000/svg"
-          className={styles.noise}
-        >
-          <filter id="noiseFilter">
-            <feTurbulence
-              type="fractalNoise"
-              baseFrequency="0.85"
-              numOctaves="6"
-              stitchTiles="stitch"
+      <img className="w-full h-screen object-cover  " src={water} />
+      <div className="w-full  h-screen  top-0 left-0 absolute ">
+        <div className="max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center ">
+          <h1 className="sm:text-5xl text-4xl font-bold text-[#ecfcf2]">
+            Mariangel Rondan
+          </h1>
+          <h2 className="flex sm:text-3xl text-2xl pt-4 text-[#ecfcf2]">
+            {" "}
+            <TypeAnimation
+              sequence={[
+                "FullStack Developer",
+                1000,
+                "Science Lover",
+                2000,
+                "Tech Enthusiactic",
+                2000,
+              ]}
+              wrapper="div"
+              cursor={true}
+              repeat={Infinity}
+              style={{ fontSize: "1em", paddingLeft: "5px" }}
             />
-          </filter>
-
-          <rect
-            width="100%"
-            height="100%"
-            preserveAspectRatio="xMidYMid meet"
-            filter="url(#noiseFilter)"
-          />
-        </svg>
-        <div className={styles.content}>
-          <div className="max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center ">
-            <h1 className="sm:text-5xl text-4xl font-bold text-[#ecfcf2]">
-              Mariangel Rondan
-            </h1>
-            <h2 className="flex sm:text-3xl text-2xl pt-4 text-[#ecfcf2]">
-              {" "}
-              <TypeAnimation
-                sequence={[
-                  "FullStack Developer",
-                  1000,
-                  "Science Lover",
-                  2000,
-                  "Tech Enthusiactic",
-                  2000,
-                ]}
-                wrapper="div"
-                cursor={true}
-                repeat={Infinity}
-                style={{ fontSize: "1em", paddingLeft: "5px" }}
+          </h2>
+          <div className="flex justify-between pt-6 max-w-[200px] w-full">
+            <a
+              href="https://www.linkedin.com/in/mariangel-rondan"
+              target="_blank"
+              rel="noopener noreferrer"
+              className=" hover:scale-110 ease-in duration-300"
+            >
+              <FaLinkedin
+                color="white"
+                size={45}
+                style={{ cursor: "pointer" }}
               />
-            </h2>
-            <div className="flex justify-between pt-6 max-w-[200px] w-full">
-              <a
-                href="https://www.linkedin.com/in/mariangel-rondan"
-                target="_blank"
-                rel="noopener noreferrer"
-                className=" hover:scale-110 ease-in duration-300"
-              >
-                <FaLinkedin
-                  color="white"
-                  size={45}
-                  style={{ cursor: "pointer" }}
-                />
-              </a>
+            </a>
 
-              <a
-                href="https://github.com/MariangelRondan"
-                target="_blank"
-                rel="noopener noreferrer"
-                className=" hover:scale-110 ease-in duration-300"
-              >
-                <FaGithub
-                  color="white"
-                  size={45}
-                  style={{ cursor: "pointer" }}
-                />
-              </a>
+            <a
+              href="https://github.com/MariangelRondan"
+              target="_blank"
+              rel="noopener noreferrer"
+              className=" hover:scale-110 ease-in duration-300"
+            >
+              <FaGithub color="white" size={45} style={{ cursor: "pointer" }} />
+            </a>
 
-              <a
-                href={MyResume}
-                download="MariangelRondan-resume.pdf"
-                className=" hover:scale-110 ease-in duration-300"
-              >
-                <img src={resume} alt="contact Logo" className="w-12  px-0 " />
-              </a>
-            </div>
+            <a
+              href={MyResume}
+              download="MariangelRondan-resume.pdf"
+              className=" hover:scale-110 ease-in duration-300"
+            >
+              <img src={resume} alt="contact Logo" className="w-12  px-0 " />
+            </a>
           </div>
-        </div>
-      </div>
-      <div className={styles.gradientBg}>
-        <svg
-          viewBox="0 0 100vw 100vw"
-          xmlns="http://www.w3.org/2000/svg"
-          className={styles.noiseBg}
-        >
-          <filter id="noiseFilterBg">
-            <feTurbulence
-              type="fractalNoise"
-              baseFrequency="0.6"
-              stitchTiles="stitch"
-            />
-          </filter>
-
-          <rect
-            width="100%"
-            height="100%"
-            preserveAspectRatio="xMidYMid meet"
-            filter="url(#noiseFilterBg)"
-          />
-        </svg>
-        <svg xmlns="http://www.w3.org/2000/svg" className={styles.svgBlur}>
-          <defs>
-            <filter id="goo">
-              <feGaussianBlur
-                in="SourceGraphic"
-                stdDeviation="10"
-                result="blur"
-              />
-              <feColorMatrix
-                in="blur"
-                mode="matrix"
-                values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8"
-                result="goo"
-              />
-              <feBlend in="SourceGraphic" in2="goo" />
-            </filter>
-          </defs>
-        </svg>
-        <div className={styles.gradientsContainer}>
-          <div className={styles.g1}></div>
-          <div className={styles.g2}></div>
-          <div className={styles.g3}></div>
-          <div className={styles.g4}></div>
-          <div className={styles.g5}></div>
-          <div id="interactive" className={styles.interactive}></div>
         </div>
       </div>
     </div>
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,112 +1,174 @@
-import React, {useState} from 'react'
-import sidebarLogo from '../assets/sidebar.png'
-// import contact from '../assets/contact-logo.png'
-import Work from "../assets/Work.svg"
-import Projects from "../assets/Projects.svg"
-import Contact from "../assets/ContactMe.svg"
-import About from "../assets/About_Me.svg"
-import skills from "../assets/skills.svg"
+import React, { useState } from "react";
+import sidebarLogo from "../assets/sidebar.png";
 
-
-
-const SideBar = () =>{
-const [nav, setNav] =  useState(false)
-const handleNav= ( ) => {
-    setNav(!nav)
-}
-function scrollToElement(elementId) {
+const SideBar = () => {
+  const [nav, setNav] = useState(false);
+  const handleNav = () => {
+    setNav(!nav);
+  };
+  function scrollToElement(elementId) {
     let element = document.getElementById(elementId);
     if (element) {
-        element.scrollIntoView({ behavior: 'smooth' });
+      element.scrollIntoView({ behavior: "smooth" });
     }
-    if(nav){
-        handleNav()
+    if (nav) {
+      handleNav();
     }
-}
+  }
 
+  return (
+    <div>
+      <img
+        onClick={handleNav}
+        src={sidebarLogo}
+        alt="sidebar Logo"
+        className="absolute top-4 right-4 z-[99] box-border h-15 w-12  md:hidden"
+      />
+      {nav ? (
+        <div className="fixed w-full h-screen pt-20 bg-white/90 flex flex-col justify-center items-center z-20">
+          <div
+            onClick={() => {
+              scrollToElement("about");
+            }}
+            className="w-[75%] h-14 flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+          >
+            <span className="pl-4  px-0">About</span>
+          </div>
+          <div
+            onClick={() => {
+              scrollToElement("work");
+            }}
+            className="w-[75%] h-14 flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+          >
+            <span className="pl-4  px-0">Work</span>
+          </div>
+          <div
+            onClick={() => {
+              scrollToElement("projects");
+            }}
+            className="w-[75%] h-14 flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+          >
+            <span className="pl-4  px-0">Projects</span>
+          </div>
 
+          <div
+            onClick={() => {
+              scrollToElement("skills");
+            }}
+            className="w-[75%] h-14 flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+          >
+            <span className="pl-4  px-0">Skills</span>
+          </div>
 
-    return (
-        <div>
-<img onClick={handleNav} src={sidebarLogo} alt='sidebar Logo' className='absolute top-4 right-4 z-[99] box-border h-15 w-12  md:hidden'/>  
-        {
-    nav ? (
-    <div className='fixed w-full h-screen pt-20 bg-white/90 flex flex-col justify-center items-center z-20'>
+          <div
+            onClick={() => {
+              scrollToElement("contact");
+            }}
+            className="w-[75%] h-14 flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+          >
+            <span className="pl-4  px-0">Contact me</span>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+      <div className=" md:block  fixed top-[25%] z-10 text-white ">
+        <div className="flex flex-col">
+          <div
+            onClick={() => {
+              scrollToElement("about");
+            }}
+            className="grid grid-cols-2 group/item   rounded-full ml-4 m-2 p-2 cursor-pointer  scale-110 ease-in duration-300 "
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="15"
+              height="15"
+              viewBox="0 0 100 100"
+            >
+              <path
+                d="M50 10C50 10 20 40 20 60C20 75 32 90 50 90C68 90 80 75 80 60C80 40 50 10 50 10Z"
+                fill="#A3DCE4"
+              />
+            </svg>
+          </div>
 
-        <div onClick={()=>{scrollToElement("about")}} className='w-[75%] h-14 flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
-        <img src={About} alt='contact Logo' className='w-10  px-0  ' />
-        <span className='pl-4  px-0'>About</span>
-        </div> 
-        <div onClick={()=>{scrollToElement("work")}} className='w-[75%] h-14 flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
-        <img src={Work} alt='contact Logo' className='w-10  px-0 ' />
-        <span className='pl-4  px-0'>Work</span>
-        </div> 
-        <div onClick={()=>{scrollToElement("projects")}} className='w-[75%] h-14 flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
-        <img src={Projects} alt='contact Logo' className='w-10  px-0 '/>
-        <span className='pl-4  px-0'>Projects</span>
-        </div> 
-       
-        
-        <div onClick={()=>{scrollToElement("skills")}} className='w-[75%] h-14 flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
-        <img src={skills} alt='contact Logo' className='w-10  px-0 '/>
-        <span className='pl-4  px-0'>Skills</span>
-        </div>  
-       
-        <div onClick={()=>{scrollToElement("contact")}} className='w-[75%] h-14 flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
-        <img src={Contact} alt='contact Logo' className='w-10  px-0 '/>
-        <span className='pl-4  px-0'>Contact me</span>
-        </div>  
-
+          <div
+            onClick={() => {
+              scrollToElement("work");
+            }}
+            className="grid grid-cols-2 group/item rounded-full ml-4  m-2 p-2 cursor-pointer scale-110 ease-in duration-300"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="15"
+              height="15"
+              viewBox="0 0 100 100"
+            >
+              <path
+                d="M50 10C50 10 20 40 20 60C20 75 32 90 50 90C68 90 80 75 80 60C80 40 50 10 50 10Z"
+                fill="#A3DCE4"
+              />
+            </svg>{" "}
+          </div>
+          <div
+            onClick={() => {
+              scrollToElement("projects");
+            }}
+            className="grid grid-cols-2 group/item rounded-full ml-4 m-2 p-2 cursor-pointer scale-110 ease-in duration-300"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="15"
+              height="15"
+              viewBox="0 0 100 100"
+            >
+              <path
+                d="M50 10C50 10 20 40 20 60C20 75 32 90 50 90C68 90 80 75 80 60C80 40 50 10 50 10Z"
+                fill="#A3DCE4"
+              />
+            </svg>
+          </div>
+          <div
+            onClick={() => {
+              scrollToElement("skills");
+            }}
+            className="grid grid-cols-2 group/item rounded-full ml-4  m-2 p-2 cursor-pointer scale-110 ease-in duration-300"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="15"
+              height="15"
+              viewBox="0 0 100 100"
+            >
+              <path
+                d="M50 10C50 10 20 40 20 60C20 75 32 90 50 90C68 90 80 75 80 60C80 40 50 10 50 10Z"
+                fill="#A3DCE4"
+              />
+            </svg>
+          </div>
+          <div
+            onClick={() => {
+              scrollToElement("contact");
+            }}
+            className="grid grid-cols-2 group/item rounded-full ml-4  m-2 p-2 cursor-pointer scale-110 ease-in duration-300"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="15"
+              height="15"
+              viewBox="0 0 100 100"
+            >
+              <path
+                d="M50 10C50 10 20 40 20 60C20 75 32 90 50 90C68 90 80 75 80 60C80 40 50 10 50 10Z"
+                fill="#A3DCE4"
+              />
+            </svg>
+          </div>
+        </div>
+      </div>
     </div>
-) : (
-    ''
-)}
-<div className=' md:block hidden fixed top-[25%] z-10 text-white '>
-<div className='flex flex-col'>
+  );
+};
 
-    <div onClick={()=>{scrollToElement("about")}} className='grid grid-cols-2 group/item   rounded-full ml-4 m-2 p-2 cursor-pointer hover:bg-[#e9a47a] scale-110 ease-in duration-300 ' >
-    <img src={About} alt='contact Logo' className='w-10  px-0  ' />
-    <p className='group/edit invisible group-hover/item:visible mt-2'>About me</p>
-    </div>
-
-    <div  onClick={()=>{scrollToElement("work")}} className='grid grid-cols-2 group/item rounded-full ml-4  m-2 p-2 cursor-pointer hover:bg-[#e9a47a] scale-110 ease-in duration-300' >
-    <img src={Work} alt='contact Logo' className='w-10  px-0 ' />
-    <p className='group/edit invisible group-hover/item:visible mt-2'>Work</p>
-
-    </div>
-    <div  onClick={()=>{scrollToElement("projects")}} className='grid grid-cols-2 group/item rounded-full ml-4 m-2 p-2 cursor-pointer hover:bg-[#e9a47a] scale-110 ease-in duration-300' >
-    <img src={Projects} alt='contact Logo' className='w-10  px-0 '/>
-    <p className='group/edit invisible group-hover/item:visible mt-2'>Projects</p>
-
-    </div>
-    <div  onClick={()=>{scrollToElement("skills")}} className='grid grid-cols-2 group/item rounded-full ml-4  m-2 p-2 cursor-pointer hover:bg-[#e9a47a] scale-110 ease-in duration-300' >
-    <img src={skills} alt='contact Logo' className='w-10  px-0 '/>
-    <p className='group/edit invisible group-hover/item:visible mt-2 px-0 '>Skills</p>
-
-    </div>
-    <div  onClick={()=>{scrollToElement("contact")}} className='grid grid-cols-2 group/item rounded-full ml-4  m-2 p-2 cursor-pointer hover:bg-[#e9a47a] scale-110 ease-in duration-300' >
-    <img src={Contact} alt='contact Logo' className='w-10  px-0 '/>
-    <p className='group/edit invisible group-hover/item:visible mt-2 px-0 '>Contact me</p>
-
-    </div>
-    
-
-
-
-
-
-</div>
-
-
-
-</div>
-
-
-
-
-
-</div> 
-    )
-}
-
-export default SideBar
+export default SideBar;
